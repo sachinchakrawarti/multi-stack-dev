8dd25227fc60e9626f120ce35a56c85ea876da23
Sachin Chakrawarti <sachinchakrawarti69@gmail.com>
2026-07-18 14:21:43 +0530

    [#118] ⚡ Update 3 JavaScript code

---
+++ learn/03_frontend_technologies/04_javascript_frameworks/03_react/00_introduction/02_code/01_cdn_template/componets/index.js
@@ -0,0 +1,13 @@
+/**
+ * Component Exports Index
+ * Central export point for all components
+ */
+
+export { default as Header } from './Header.jsx';
+export { default as Footer } from './Footer.jsx';
+export { default as UserCard } from './UserCard.jsx';
+export { default as Button } from './Button.jsx';
+export { default as Card } from './Card.jsx';
+export { default as Alert } from './Alert.jsx';
+export { default as ProductList } from './ProductList.jsx';
+export { default as Counter } from './Counter.jsx';
