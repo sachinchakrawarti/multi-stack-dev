3edb3d042f91f8dd58477cc3c208a1ae3cf6fa42
Sachin Chakrawarti <sachinchakrawarti69@gmail.com>
2026-07-22 16:48:58 +0530

    [#26] ⚡ Update 3 JavaScript code

---
+++ learn/02_programming_languages/02_scripting/javascript/06_objects/05_mcq/questions/mcq_01.js
@@ -0,0 +1,49 @@
+const questions=[
+
+{
+question:"What is an object in JavaScript?",
+options:[
+"A primitive value",
+"A collection of key-value pairs",
+"A loop",
+"A class"
+],
+answer:"A collection of key-value pairs"
+},
+
+{
+question:"Which brackets create an object?",
+options:[
+"()",
+"[]",
+"{}",
+"<>"
+],
+answer:"{}"
+},
+
+{
+question:"Objects store data as?",
+options:[
+"Lists",
+"Arrays",
+"Key-value pairs",
+"Functions"
+],
+answer:"Key-value pairs"
+},
+
+{
+question:"Which keyword is commonly used?",
+options:[
+"var",
+"const",
+"if",
+"switch"
+],
+answer:"const"
+}
+
+];
+
+loadQuiz(questions);
