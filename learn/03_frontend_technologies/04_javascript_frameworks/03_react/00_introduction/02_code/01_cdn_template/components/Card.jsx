91532d7bf3e529a9dd73b449fe41e35ed3579b38
Sachin Chakrawarti <sachinchakrawarti69@gmail.com>
2026-07-24 16:39:08 +0530

    [#24] ⚛️ Update 3 React component

---
+++ learn/03_frontend_technologies/04_javascript_frameworks/03_react/00_introduction/02_code/01_cdn_template/components/Card.jsx
@@ -0,0 +1,15 @@
+function Card(){
+
+    return(
+
+        <div className="card">
+
+            <h2>Card Component</h2>
+
+            <p>This is a reusable card.</p>
+
+        </div>
+
+    );
+
+}
