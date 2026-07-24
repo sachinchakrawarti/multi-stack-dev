bb8f7b4ae8869191ce156f6df2d67699b44f3774
Sachin Chakrawarti <sachinchakrawarti69@gmail.com>
2026-07-24 16:38:49 +0530

    [#22] ⚛️ Update 3 React component

---
+++ learn/03_frontend_technologies/04_javascript_frameworks/03_react/00_introduction/02_code/01_cdn_template/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header(){
+
+    return(
+
+        <header className="card">
+
+            <h1>React CDN Template</h1>
+
+            <p>Reusable Header Component</p>
+
+        </header>
+
+    );
+
+}
